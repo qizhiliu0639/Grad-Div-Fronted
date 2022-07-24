ec783eac19b9abd9ff9f20dc0a266f5aad4267f0
liuzhiqi <j419230944@hotmail.com>
2022-07-24 15:41:12 +0800

    try to fix get function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import {
+  Table,
+  Tooltip,
+  message,
+} from "antd";
+import { useEffect, useState } from "react";
 import './App.css';
+import axios from "axios";
+
+// syncUserList: Get the list of all users
+const syncUserList = async (setUserList) => {
+  try {
+      const userData = await axios.get("http://localhost:9000/api/todos");
+      if (userData.data["success"] !== 0) {
+          message.error(`Get user list error: ${userData.data["message"]}`);
+          return
+      }
+      const users = userData.data.map(v => {
+          return {
+              name: v.Name,
+          };
+      });
+      setUserList(users);
+  } catch (error) {
+      if (error.response !== undefined && error.response.data.message !== undefined) {
+          message.error(`Get user list error: ${error.response.data.message}`);
+      } else {
+          message.error(`Get user list error: ${error}`);
+      }
+  }
+}
 
 function App() {
+  const [userList, setUserList] = useState([])
+    const [currentUser, setCurrentUser] = useState({
+        ApplicationNum: 0,
+        Name: "",
+        // status: 0 for normal user, 1 for admin
+        Gender: "",
+    })
+    const [quotaVisible, setQuotaVisible] = useState(false);
+
+    useEffect(() => {
+        syncUserList(setUserList);
+    }, []);
+  const columns = [
+    {
+        title: 'Username',
+        key: 'username',
+        width: "15%",
+        ellipsis: {
+            showTitle: false,
+        },
+        render: (record) => {
+            return (
+                <Tooltip placement="topLeft" title={record.name}>
+                    <span>{record.name}</span>
+                </Tooltip>
+            )
+        },
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="usersTableContainer">
+      <Table id="favoritesTable" columns={columns} dataSource={userList} />
     </div>
-  );
+  )
 }
 
 export default App;
