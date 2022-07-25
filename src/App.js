b4ff1fce6fbdbb5ef8decdd6c56492e5885e7533
liuzhiqi <j419230944@hotmail.com>
2022-07-25 13:39:55 +0800

    show name on the page

--- src/App.js
+++ src/App.js
@@ -6,65 +6,31 @@ import {
 import { useEffect, useState } from "react";
 import './App.css';
 import axios from "axios";
+import { render } from "@testing-library/react";
 
-// syncUserList: Get the list of all users
-const syncUserList = async (setUserList) => {
-  try {
-      const userData = await axios.get("http://localhost:9000/api/todos");
-      if (userData.data["success"] !== 0) {
-          message.error(`Get user list error: ${userData.data["message"]}`);
-          return
-      }
-      const users = userData.data.map(v => {
-          return {
-              name: v.Name,
-          };
-      });
-      setUserList(users);
-  } catch (error) {
-      if (error.response !== undefined && error.response.data.message !== undefined) {
-          message.error(`Get user list error: ${error.response.data.message}`);
-      } else {
-          message.error(`Get user list error: ${error}`);
-      }
+import React from 'react';
+
+export default class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons: []
+    }
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:9000/api/todos")
+    .then(res => {
+      const persons = res.data;
+      this.setState({ persons });
+    })
+  };
+
+  render() {
+    return (
+      <ul>
+        { this.state.persons.map(person => <li>{person.Name}</li>)}
+      </ul>
+    )
   }
 }
-
-function App() {
-  const [userList, setUserList] = useState([])
-    const [currentUser, setCurrentUser] = useState({
-        ApplicationNum: 0,
-        Name: "",
-        // status: 0 for normal user, 1 for admin
-        Gender: "",
-    })
-    const [quotaVisible, setQuotaVisible] = useState(false);
-
-    useEffect(() => {
-        syncUserList(setUserList);
-    }, []);
-  const columns = [
-    {
-        title: 'Username',
-        key: 'username',
-        width: "15%",
-        ellipsis: {
-            showTitle: false,
-        },
-        render: (record) => {
-            return (
-                <Tooltip placement="topLeft" title={record.name}>
-                    <span>{record.name}</span>
-                </Tooltip>
-            )
-        },
-    }
-  ]
-  return (
-    <div id="usersTableContainer">
-      <Table id="favoritesTable" columns={columns} dataSource={userList} />
-    </div>
-  )
-}
-
-export default App;
